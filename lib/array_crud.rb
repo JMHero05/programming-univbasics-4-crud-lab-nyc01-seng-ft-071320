124168eb321f28ed04acf86c2578d8a1d2390060
Jake Mills <jake.mills.05@gmail.com>
2020-07-01 02:25:38 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  array = mushroom_kingdom << "Bowser"
+  create_an_array << "Bowser"
 end
 
 def add_element_to_start_of_array(array, element)
