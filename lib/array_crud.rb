5de8624d0f5a3256a9927788f9c6397366c5955d
Jake Mills <jake.mills.05@gmail.com>
2020-07-01 02:22:23 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  somewhat_empty_array << "Toad"
+  muschroom_kingdom << "Bowser"
 end
 
 def add_element_to_start_of_array(array, element)
